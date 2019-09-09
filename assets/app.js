8fc90411fd8c211921edce1d02af3e29302daf6c
Chris Lucynski <chris.lucynski@gmail.com>
2019-09-08 18:04:04 -0600

    changed background

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,23 @@
 
 // toggle classes/data-attributes, especially for fading and hiding
 
-function imageClick(){
 
-}
 $('img').click(function() {
-    $('this').attr('id')
     console.log($('this'))
+    $('this').attr('id')
 });
+
+var skillsBars = $('.skills')
+
+$(skillsBars).mouseover(function(){
+    $('this').querySelector().
+    $(skillsBars).removeClass('w3-dark-grey')
+})
+
+$(skillsBars).mouseout(function(){
+    $(skillsBar).addClass('w3-dark-grey')
+})
+
+$('.skills').hover(function(){$(this).toggleClass('skills_hover');});
+
+
