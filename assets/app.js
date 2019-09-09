ae3f0c8e43f3691fd5201e0135ab71e884aa936a
Chris Lucynski <chris.lucynski@gmail.com>
2019-09-09 17:53:02 -0600

    updated About Me section

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,5 @@
-// dynamically create some of the content on the page
-
-// toggle classes/data-attributes, especially for fading and hiding
 
 
-$('img').click(function() {
-    console.log($('this'))
-    $('this').attr('id')
-});
 
-var skillsBars = $('.skills')
-
-$(skillsBars).mouseover(function(){
-    $('this').querySelector().
-    $(skillsBars).removeClass('w3-dark-grey')
-})
-
-$(skillsBars).mouseout(function(){
-    $(skillsBar).addClass('w3-dark-grey')
-})
-
-$('.skills').hover(function(){$(this).toggleClass('skills_hover');});
 
 
